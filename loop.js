a6c86d0b2d6f47cf4749c8ca36c2a682ad96295e
Sabbir <webexpertsabbir@gmail.com>
2022-08-31 20:37:46 +0600

    for in loop

--- loop.js
+++ loop.js
@@ -1,6 +1,6 @@
 const bottle = {color: 'Yellow', price: 50, isCliend: true, capacity: 1};
 const keys = Object.keys(bottle);
-console.log(keys);
+// console.log(keys);
 
 /* 3 way read object property
 bottle.color
@@ -9,6 +9,19 @@ bottle[key]
 
 */
 for(const key of keys){
-    console.log(key, bottle[key]);
+    // console.log(key, bottle[key]);
 }
 
+for(const key in bottle){
+    const value = bottle[key];
+    // console.log(value, key);
+}
+
+
+
+const pair = Object.entries(bottle);
+// console.log(pair);
+
+for(const [key, value] of Object.entries(bottle)){
+    console.log(key, value);
+}
