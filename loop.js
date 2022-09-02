89030d22264651c58c7c4181156ea68b01453a44
Sabbir <webexpertsabbir@gmail.com>
2022-09-02 10:48:50 +0600

    add unicq catagori

--- loop.js
+++ loop.js
@@ -23,5 +23,13 @@ const pair = Object.entries(bottle);
 // console.log(pair);
 
 for(const [key, value] of Object.entries(bottle)){
-    console.log(key, value);
+    // console.log(key, value);
 }
+
+// const obj ={a:1, b:7, c:3, length:2};
+// console.log(Object.keys(obj).length);
+
+// const obj1= {module: 35, video:2}; const obj2= {module: 35, video:2};
+// console.log(obj1 === obj2);
+
+const getGirlFriend= (name = "chokina")=>"name"; console.log(getGirlFriend()); 
